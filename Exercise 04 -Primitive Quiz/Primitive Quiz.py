4989cfa22435924ca6f49f6a34d991c228917833
JophMythic <joph2907@gmail.com>
2025-11-24 18:53:13 +0400

    Country and Capital
    Code that helps the user find out if the capital of a country was right

---
+++ Exercise 04 -Primitive Quiz/Primitive Quiz.py
@@ -0,0 +1,9 @@
+country = "France"
+correct_capital = "paris"
+print("What is the capital of France")
+user_answer = input("> ")
+if user_answer == correct_capital:
+    print("correct answer")
+else:
+    print("wrong answer")
+    
