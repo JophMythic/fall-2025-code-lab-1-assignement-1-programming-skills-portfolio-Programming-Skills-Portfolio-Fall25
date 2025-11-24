67235d9f0367330c7f137be657bd775512afb968
JophMythic <joph2907@gmail.com>
2025-11-24 13:18:05 +0400

    Biography using codes
    Using code to get information

---
+++ Exercise 03 - Biography/Biography.py
@@ -0,0 +1,13 @@
+my_name = "Joph"         
+my_hometown = "Ajman"    
+my_age = 18              
+bio = {
+    "name": my_name,
+    "hometown": my_hometown,
+    "age": my_age
+}
+print(
+    f"Name: {bio['name']}\n"
+    f"Hometown: {bio['hometown']}\n"
+    f"Age: {bio['age']}"
+)
